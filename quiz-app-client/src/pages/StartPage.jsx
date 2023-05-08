48b7504367b4bb4079344fa56c23a8f6be01f0d1
ibldzn <51160226+ibldzn@users.noreply.github.com>
2023-05-08 18:56:16 +0700

    feat: added background music

---
+++ quiz-app-client/src/pages/StartPage.jsx
@@ -0,0 +1,42 @@
+import { useRef, useEffect } from "react";
+
+const StartPage = ({ setUsername }) => {
+  const bgm = new Audio("/bgm.mp3");
+
+  useEffect(() => {
+    bgm.loop = true;
+    bgm.load();
+    bgm.play();
+
+    return () => {
+      bgm.pause();
+    };
+  }, []);
+
+  const inputRef = useRef();
+
+  const handleClick = () => {
+    if (inputRef.current.value) {
+      setUsername(inputRef.current.value);
+    }
+  };
+
+  return (
+    <div className="flex flex-col items-center w-fit justify-center">
+      <input
+        ref={inputRef}
+        className="border-2 border-gray-300 p-2 rounded-lg"
+        type="text"
+        placeholder="Masukkan nama anda"
+      />
+      <button
+        onClick={handleClick}
+        className="border-2 border-blue-800 w-full p-2 rounded-lg mt-2 text-white"
+      >
+        Start
+      </button>
+    </div>
+  );
+};
+
+export default StartPage;
