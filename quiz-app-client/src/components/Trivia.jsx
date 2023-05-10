8c1cdcc2653d7a2a10c894277583c08fcfac3df7
ibldzn <51160226+ibldzn@users.noreply.github.com>
2023-05-10 13:09:20 +0700

    use images in trivia component

--- quiz-app-client/src/components/Trivia.jsx
+++ quiz-app-client/src/components/Trivia.jsx
@@ -2,11 +2,13 @@ const Trivia = ({ question }) => {
   return (
     <div className="flex flex-col items-center justify-end h-full text-xl">
       {question.image && (
-        <img
-          src={question.image}
-          alt={question.question}
-          className="w-1/2 mb-4"
-        />
+        <div className="w-64 h-64">
+          <img
+            src={question.image}
+            alt={question.question}
+            className="w-full h-full object-contain mb-4"
+          />
+        </div>
       )}
       <div className="flex items-center justify-center bg-gradient-to-b from-[#100241] to-black p-8 border-2 border-white rounded-xl h-40 w-full">
         <div className="h-full overflow-y-auto">{question.question}</div>
@@ -15,9 +17,9 @@ const Trivia = ({ question }) => {
         {question.choices.map((choice) => (
           <div
             key={choice}
-            className="w-2/5 h-12 p-2 m-2 border border-white rounded-xl bg-gradient-to-b from-[#0e0124] to-[#22074d] font-light cursor-pointer text-center hover:bg-blue-500"
+            className="w-2/5 h-12 p-3 m-2 border border-white rounded-xl bg-gradient-to-b from-[#0e0124] to-[#22074d] font-light cursor-pointer text-center hover:bg-blue-500"
           >
-            <div className="h-full overflow-y-auto truncate">{choice}</div>
+            <div className="h-full overflow-y-auto">{choice}</div>
           </div>
         ))}
       </div>
