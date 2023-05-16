43c9d7b8a1a6273b1b8715e072784cfa0318e2f0
ibldzn <51160226+ibldzn@users.noreply.github.com>
2023-05-16 08:47:18 +0700

    update timer component

--- quiz-app-client/src/components/Timer.jsx
+++ quiz-app-client/src/components/Timer.jsx
@@ -1,11 +1,29 @@
-import useTimer from "../hooks/useTimer";
+import { useEffect, useState } from "react";
 
-const Timer = ({ timeoutSec, onTimeout = () => {} }) => {
-  const time = useTimer(timeoutSec, onTimeout);
+const Timer = ({ setTimeout, questionNumber, pauseTimer }) => {
+  const [timer, setTimer] = useState(30);
 
-  return (
-    <div className="flex items-center justify-center text-5xl p-12">{time}</div>
-  );
+  useEffect(() => {
+    if (timer === 0) {
+      return setTimeout(true);
+    }
+
+    const interval = setInterval(() => {
+      setTimer((prev) => prev - 1);
+    }, 1000);
+
+    if (pauseTimer) {
+      clearInterval(interval);
+    }
+
+    return () => clearInterval(interval);
+  }, [timer, setTimeout, pauseTimer]);
+
+  useEffect(() => {
+    setTimer(30);
+  }, [questionNumber]);
+
+  return <div className="flex items-center text-5xl p-12">{timer}</div>;
 };
 
 export default Timer;
